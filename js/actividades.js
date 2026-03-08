1c101b1287a3ccd0e9b6b7682b6ec45088b270e3
2024011zi-ops <2024011zi@unicla.edu.mx>
2026-03-07 19:02:04 -0600

    Update actividades.js

--- js/actividades.js
+++ js/actividades.js
@@ -1,1 +1,45 @@
+// selección de alimentos
 
+let alimentoSeleccionado = null;
+
+document.querySelectorAll(".alimento").forEach(function(alimento){
+
+alimento.addEventListener("click", function(e){
+
+e.stopPropagation();
+
+// si ya está seleccionado se deselecciona
+
+if(alimentoSeleccionado === this){
+
+this.classList.remove("seleccionado");
+alimentoSeleccionado = null;
+return;
+
+}
+
+// quitar selección anterior
+
+if(alimentoSeleccionado){
+alimentoSeleccionado.classList.remove("seleccionado");
+}
+
+// seleccionar nuevo
+
+this.classList.add("seleccionado");
+alimentoSeleccionado = this;
+
+});
+
+});
+
+// clic fuera para deseleccionar
+
+document.addEventListener("click", function(){
+
+if(alimentoSeleccionado){
+alimentoSeleccionado.classList.remove("seleccionado");
+alimentoSeleccionado = null;
+}
+
+});
